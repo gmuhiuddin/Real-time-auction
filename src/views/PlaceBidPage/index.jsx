feac6a5d3f4b8cd8b1154ab17e89dccef8371b44
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-06-09 10:42:48 +0500

    add soem blaaa

--- src/views/PlaceBidPage/index.jsx
+++ src/views/PlaceBidPage/index.jsx
@@ -18,82 +18,88 @@ function PlaceBidPage() {
     const [successMsg, setSuccessMsg] = useState();
     const authInfo = useSelector(res => res.userInfo.auth);
     const navigate = useNavigate();
-    
+
     useEffect(() => {
-            getProductFromDb(productId, setProduct);
+        getProductFromDb(productId, setProduct);
         getBids(productId, setBids);
     }, []);
-
+    
     const handlePlaceBid = async (e) => {
         e.preventDefault();
         setSuccessMsg();
         setErrMsg();
 
-        try {
-            await placeABid(e.target[0].value, productId, authInfo?.uid, product.uid);
+        if (authInfo?.uid && product?.uid != authInfo.uid && bids && authInfo?.uid != bids[0]?.uid && bids[0].bidAmount < e.target[0].value) {
+            try {
+                await placeABid(e.target[0].value, productId, authInfo?.uid, product.uid);
 
-            setSuccessMsg("Bid placed successfully");
-            e.target[0].value = "";
-        } catch (err) {
-            setErrMsg("Some thing went wrong");
-            e.target[0].value = "";
+                setSuccessMsg("Bid placed successfully");
+                e.target[0].value = "";
+            } catch (err) {
+                setErrMsg("Some thing went wrong");
+                e.target[0].value = "";
+            };
+        } else {
+            alert("Some thing went worng");
+            window.location.reload();
         };
+
     };
 
-    if(!product) return <Loader />;
-    
-    if(!Object.keys(product).length) return navigate('/404');
+    if (!product) return <Loader />;
+
+    if (!Object.keys(product).length) return navigate('/404');
 
     return (
         <div className='detail-page-main-container'>
             <span onClick={() => navigate('/')} className='back-btn'><BsArrowLeft size={31} /></span>
-                <>
-                    <div className="product-detail-container">
-                        <ImageSlider images={product.images} />
-                        <h2>{product.title}</h2>
-                        <h3>${product.price}</h3>
-                        <span>{product.description}</span>
-                    </div>
-                    <div className="place-bid-container">
-                        <form onSubmit={handlePlaceBid}>
-                            <input type='number' placeholder='Please enter bid amount' required min={product.price} />
-                            <button disabled={authInfo?.uid ? bids && authInfo?.uid == bids[0]?.uid : true} type='submit'>Bid</button>
-                        </form>
-                        <div>
-                            {bids ?
-                                <table>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Price</th>
-                                        <th>Time</th>
-                                        <th>date</th>
-                                    </tr>
-                                    {bids.map(element => {
-                                        return (
+            <>
+                <div className="product-detail-container">
+                    <ImageSlider images={product.images} />
+                    <h2>{product.title}</h2>
+                    <h3>${product.price}</h3>
+                    <span>{product.description}</span>
+                </div>
+                <div className="place-bid-container">
+                    <form onSubmit={handlePlaceBid}>
+                        <input type='number' placeholder='Please enter bid amount' required min={product.price} />
+                        <button disabled={authInfo?.uid ? product?.uid == authInfo.uid ? true : bids && authInfo?.uid == bids[0]?.uid : true} type='submit'>Bid</button>
+                    </form>
+                    <div>
+                        {bids ?
+                            <table>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Time</th>
+                                    <th>date</th>
+                                </tr>
+                                {bids.map(element => {
+                                    return (
 
-                                            <tr>
-                                                <td>{element.username}</td>
-                                                <td>${element.bidAmount}</td>
-                                                <td>
-                                                    {dayjs(element.time?.toDate()).format(
-                                                        "hh:mm"
-                                                    )}
-                                                </td>
-                                                <td>
-                                                    {dayjs(element.time?.toDate()).format(
-                                                        "DD-MM"
-                                                    )}
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                </table>
-                                :
-                                <p>no bids yet</p>
-                            }
-                        </div>
+                                        <tr>
+                                            <td>{element.username}</td>
+                                            <td>${element.bidAmount}</td>
+                                            <td>
+                                                {dayjs(element.time?.toDate()).format(
+                                                    "hh:mm"
+                                                )}
+                                            </td>
+                                            <td>
+                                                {dayjs(element.time?.toDate()).format(
+                                                    "DD-MM"
+                                                )}
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </table>
+                            :
+                            <p>no bids yet</p>
+                        }
                     </div>
-                </>
+                </div>
+            </>
             {errMsg && <CutomAlert isErrMsg={true} txt={errMsg} />}
             {successMsg && <CutomAlert isErrMsg={false} txt={successMsg} />}
         </div>
